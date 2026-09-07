9c8ce2956ef8bb073641d67ec3354a157cbbd3ed
EDWARDAV0228 <akmalosh8484@gmail.com>
2026-09-07 18:46:17 +0500

    Add search field

--- main/admin.py
+++ main/admin.py
@@ -24,20 +24,24 @@ class NewsAdmin(admin.ModelAdmin):
             return mark_safe(f'<img src="{obj.image.url}" width="100%">')
         return '-'
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name',)
+    search_fields = ('id', 'name')
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name',)
+    search_fields = ('id', 'name')
+
 
 @admin.register(NewsLinks)
 class NewsLinksAdmin(admin.ModelAdmin):
     list_display = ('id', 'news', 'whatsapp', 'telegram', 'instagram', 'facebook')
     list_display_links = ('id', 'news',)
-
+    search_fields = ('id', 'whatsapp', 'telegram', 'instagram', 'facebook')
 # Register your models here.
